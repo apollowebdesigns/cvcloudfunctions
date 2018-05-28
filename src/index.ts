02a995f427469c08ebcbe181442315a02b1da33b
Andy Evans <andrewevans0@outlook.com>
2018-05-28 07:39:43 +0100

    updated firebase stuff

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import Response from './interfaces/response';
+
+let config = {
+    credential: admin.credential.cert('firebase-adminsdk-rmtfk@userddata.iam.gserviceaccount.com'),
+    databaseURL: 'https://userddata.firebaseio.com'
+};
+admin.initializeApp(config);
+
+// Get a reference to the database service
+let database = admin.database();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
-const responder: string = 'this uses an interface!';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
+    let test = database.ref('test');
     let arr: Response[] = new Array<Response>();
     console.log(request);
     arr.push({
         response: 'test'
-    })
-    const finalResponse: Response = {
-        response: responder
-    }
+    });
 
-    response.send(arr);
+    response.send(test);
 });
