f71b0edf79c08ad391f0aa29b00defd4a46e96de
Andy Evans <andrewevans0@outlook.com>
2018-07-09 19:58:06 +0100

    updated test

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ app.get('/store', (req, res) => {
     // Get a reference to the storage service, which is used to create references in your storage bucket
     const storage = admin.storage();
     const jsonStore = storage.bucket('gs://userddata.appspot.com/test.json');
-    res.send(jsonStore);
+    res.send(jsonStore.get());
 });
 
 app.use('/weathergraph', graphqlHTTP({
